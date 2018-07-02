e6edefa95e671052f27abca4ecfb4074905ba484
Sage Jenson <andrescuervor@gmail.com>
2018-07-03 01:51:57 +0200

    Add correct neovim Python 2 & 3 installation

--- vim/setup.sh
+++ vim/setup.sh
@@ -2,6 +2,10 @@
 
 brew install neovim
 
+pip3 install --user neovim # neovim Python 3 package
+echo " ✨✨✨✨✨ \nPlease enter your system password (to install neovim Python 2.7 package): "
+sudo easy_install neovim # neovim Python 2.7 package
+
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s ~/dotfiles/vim ~/.vim
 ln -s ~/dotfiles/vim/init.vim ~/.config/nvim/init.vim
