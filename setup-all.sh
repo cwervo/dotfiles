ea155ebcd5032639b85af01142b41f9e6556a498
Andrés Cuervo <asc@andress-mbp.attlocal.net>
2018-07-23 19:34:53 -0700

    Add python fixes for nvim installation

--- setup-all.sh
+++ setup-all.sh
@@ -1,5 +1,5 @@
 ./brew/setup-homebrew.sh
 ./git/setup-git.sh
-./zsh/setup-z.sh
 ./tmux/setup-tmux.sh
 ./vim/setup.sh
+./zsh/setup-z.sh
--- vim/setup.sh
+++ vim/setup.sh
@@ -5,7 +5,7 @@ mkdir -p ~/.config/nvim
 
 pip3 install --user neovim # neovim Python 3 package
 echo " ✨✨✨✨✨ \nPlease enter your system password (to install neovim Python 2.7 package): "
-sudo easy_install neovim # neovim Python 2.7 package
+pip2 install --user neovim # neovim Python 2.7 package
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s ~/dotfiles/vim ~/.vim
