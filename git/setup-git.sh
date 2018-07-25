fd5fea4831e67638e7f8428d2530e9760f829d5b
Andres Cuervo <andrescuervor@gmail.com>
2018-07-25 16:32:40 -0700

    Add user config to git setup script

--- git/setup-git.sh
+++ git/setup-git.sh
@@ -1,2 +1,4 @@
 git config --global core.excludesfile ~/.gitignore_global
+git config --global user.name "Andrés Cuervo"
+git config --global user.email andrescuervor@gmail.com
 ln -s ~/dotfiles/git/.gitignore_global ~/.gitignore_global
