ba8f35fe00b7e275cb9467d26195815a22bdaea4
Andrés Cuervo <andrescuervor@gmail.com>
2018-12-18 03:26:34 -0800

    Clean up brew cask & hyper install process

--- brew/setup-homebrew.rb
+++ brew/setup-homebrew.rb
@@ -1,12 +1,17 @@
 #!/usr/bin/env ruby
-# cat ~/dotfiles/brew/brew-cask-leaves.txt | xargs brew cask install
 # Install
 `/usr/bin/ruby -e "" $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)`
-['list', 'cask-list'].each{ |subbedCommand|
-  p "doing #{subbedCommand}"
+['list', 'cask list'].each{ |brewCommand|
+  p "doing #{brewCommand}"
+  subbedCommand = brewCommand.sub(' ', '-')
+  firstSubbedCommand = ''
+
+  if brewCommand.include? ' '
+    firstSubbedCommand = brewCommand.split(' ')[0]
+  end
+
   begin
-    # `cat ~/dotfiles/brew/brew-#{subbedCommand}.txt | xargs brew install 2>/dev/null`
-    `cat ~/dotfiles/brew/brew-#{subbedCommand}.txt | xargs brew install`
+    `cat ~/dotfiles/brew/brew-#{subbedCommand}.txt | xargs brew #{firstSubbedCommand} install`
   rescue
     p "whoops"
   end
@@ -14,4 +19,4 @@
 
 # Link Cask apps to their binary
 ## Hyper self-links when you first call its binary!
-`/Applications/Hyper.app/Contents/Resources/bin/hyper`
+`/Applications/Hyper.app/Contents/Resources/bin/hyper help`
--- hyper/install-hyper.rb
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env ruby
-`ln -s hyper/hyper.js ~/.hyper.js`
