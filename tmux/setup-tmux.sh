d62b6634998c64cf26634e7d1eb1c91c89d213dd
Andrés Cuervo <andrescuervor@gmail.com>
2019-06-26 12:53:42 +0200

    Re-add TPM, didn't fix tmux-continuum tho

--- tmux/setup-tmux.sh
+++ tmux/setup-tmux.sh
@@ -1,8 +1,8 @@
 #! /bin/zsh
 
-
 ln -s ~/dotfiles/tmux ~/.tmux
 ln -s ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 
 # Set up Tmux Plugin Manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tmux-resurrect ~/clone/path
